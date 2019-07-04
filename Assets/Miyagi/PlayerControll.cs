253c6e44321a88d95bc7d2009ffbe977db2f5ca4
kazuki24 <kazu2324ki@gmail.com>
2019-07-04 10:35:37 +0900

    スタートと体力追加

--- Assets/Miyagi/PlayerControll.cs
+++ Assets/Miyagi/PlayerControll.cs
@@ -1,22 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerControll : MonoBehaviour {
     
-    float time = 0;
-
+    float time;
+    public bool startFlg;
     public string buttomtype;
+    private int playerlife;
+
+    [SerializeField] Text lifetext;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        time = 0;
+        startFlg = false;
+        playerlife = 10;
+        lifetext.text = ""+playerlife;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKeyDown(KeyCode.D)) {
+        if(startFlg == true)
+        GetKeyType();
+
+        if((time += Time.deltaTime) > 1)
+        {
+            playerlife -= 1;
+            lifetext.text = "" + playerlife;
+            time = 0;
+        }
+        if(playerlife == 0)
+        {
+            SceneManager.LoadScene("Main");
+            GetComponent<StageScript>().Start();
+            GetComponent<StartScript>().Start();
+            Start();
+
+        }
+
+        Debug.Log(playerlife);
+    }
+
+    private void GetKeyType()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
             buttomtype = "right";
             GetComponent<StageScript>().PlayerMove(buttomtype);
         }
@@ -30,7 +62,5 @@ public class PlayerControll : MonoBehaviour {
             buttomtype = "buttom";
             GetComponent<StageScript>().PlayerMove(buttomtype);
         }
-
-
     }
 }
---
+++ Assets/Miyagi/StartScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartScript : MonoBehaviour {
+
+    [SerializeField]Text textStart;
+    float alpha;
+    float alphaspeed;
+
+    int time;
+	public void Start () {
+        alpha = 1;
+        time = 0;
+        alphaspeed = 0.02f;
+    }
+	
+	
+	void Update () {
+
+        if (textStart != false)
+        {
+            textStart.GetComponent<Text>().color = new Color(0, 0, 0, alpha);
+            if (alpha <= 0.3 || alpha >= 1)
+                alphaspeed *= -1;
+            alpha += alphaspeed;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<PlayerControll>().startFlg = true;
+            textStart.enabled = false;
+        }
+	}
+}
