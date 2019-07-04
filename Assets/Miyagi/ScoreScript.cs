c06ccff22b060908f63514f1eddca9887723d696
redox114514 <41351188+redox114514@users.noreply.github.com>
2019-07-04 16:03:30 +0900

    変更しました

--- Assets/Miyagi/ScoreScript.cs
+++ Assets/Miyagi/ScoreScript.cs
@@ -1,19 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreScript : MonoBehaviour {
 
     [SerializeField]int BlockCount;
     float Score;
+    public Text scoreText; // スコアの UI
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
         //ScoreScript ScoreConfig;
         //ScoreConfig = this.GetComponent<ScoreScript>();
         //ScoreConfig.AddScoreBlock();
         //ScoreConfig.AddScore();
+
+        // UI を初期化
+        Score = 0;
+        SetCountText();
 
     }
 
@@ -39,12 +45,23 @@ public class ScoreScript : MonoBehaviour {
             for (int i = 1; BlockCount > i; i++)
             {
                 Score += 100 + (50 * i);
+                // UI の表示を更新
+                SetCountText();
             }
 
             BlockCount = 0;
+            
         }
         Debug.Log(Score);
         return Score;
+    }
+
+    // UI の表示を更新する
+    void SetCountText()
+    {
+        // スコアの表示を更新
+        scoreText.text = "Count: " + Score.ToString();
+
     }
 
 }
